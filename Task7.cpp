8c50b9d281cb405b69c8abfeafbfd38c04528aad
Joe <josephmorritt@gmail.com>
2020-01-12 11:32:27 +0000

    Task7.1
    Task 7 code

--- Task7.cpp
+++ Task7.cpp
@@ -5,8 +5,16 @@ ID:
 Date:
 
 */
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <ctype.h>
 #include <iostream>
 #include <map>
+#include <string>
+#include <fstream>
+#include <cctype>
+#include <bits/stdc++.h>
 
 using namespace std;
 //__________________________________//
@@ -29,7 +37,8 @@ public:
   {
     for(map<string, string>::iterator it = newMap.begin(); it != newMap.end(); it++)
     {
-        cout << "ID " << it->first << " Name " << it->second << endl;// Print out THe member of the map
+        cout << "ID: " << it->first << " Name: " << it->second << '\n';// Print out THe member of the map
+        std::cout << " " << '\n';
       }
   }
   void clearing(){ // This inlizes the clear function
@@ -41,19 +50,53 @@ public:
   newMap.erase(pt); // removes the value at the current postion
   }
 };
+class errors : public studentinfo{
+public:
+	int n;
+  string errorset;
+  string errorset2;
+  void Iderror()
+	{
+	for (size_t n = 0; n < errorset.length(); n++)
+		{
+		if (!isdigit( errorset[ n ] ))
+			{
+			std::cout << "Invalied id" << '\n';
+      exit(0);
+			}
+		}
+	}
+  void Nameerror()
+  {
+    for (size_t n = 0; n < errorset2.length(); n++)
+  		{
+  		if (!isdigit( errorset2[ n ] ))
+  			{
+            return;
+  			}
+     else{
+       std::cout << "Invalied Name" << '\n';
+       exit(0);
+
+     }
+  	}
+   }
+};
+
 //__________________________________//
 //             Main loop
 int main()
 {
 
     studentinfo info; // Class objects
-    info.id = "default";// Data member access.
+		errors error;
+    info.id ;// Data member access.
     info.firstname;
     string input;// varible delration
 
 //__________________________________//
 //            While loop
-    while (input !="quit") {
+  while (input !="quit") {
     // Options Menu
     std::cout << "what would you like to do" << '\n';
     std::cout << "1. add new student" << '\n';
@@ -76,13 +119,23 @@ int main()
       cout << "Enter " << " Id and name" << endl; // ask  the user for ID number and name.
       cout << "Id ";
       cin >> info.id; // taks in the given id value
-      cout << "Name ";
+      error.errorset = info.id;
+
+      error.Iderror();
+
+      cout << "Name " << '\n';
       cin >> info.firstname; // takes in the name given
+      std::cout << " " << '\n';
+      error.errorset2 = info.firstname;
+
+      error.Nameerror();
+
       info.insert();// call the insert function
      }
      if(input == "2"){
-       std::cout << "Enter Id to remove" << '\n';
+       std::cout << "Enter Id to remove";
        std::cin >> info.id;
+
        info.Remove(); // calls the remove function.
      }
     if(input == "3"){
@@ -95,6 +148,8 @@ int main()
       info.clearing();
 
     }
-    }
+
+
+  }
 
 }
